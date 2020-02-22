ef699528eba72521ec493d5dd99359e0b6ef177c
Santiago Tamayo <stamayol@unal.edu.co>
2020-02-22 11:30:08 -0500

    probar
    colocar libreria gson
    organizar data

--- RAPPI2/src/BaseDatos/Data.java
+++ RAPPI2/src/BaseDatos/Data.java
@@ -1,12 +1,17 @@
 package BaseDatos;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+
+import com.google.gson.*;
 
 import Administracion.Perfil;
-
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
 
 public class Data {
 	
@@ -15,7 +20,79 @@ public class Data {
 	private static final String filepathRestaurantes = "C:\\Users\\mythe\\git\\repository\\RAPPI2\\temp\\restaurantes";
 	
 	
-	public static void WritePerfilToFile(Object obj) {
+	
+	
+	/* forma de usar el metodo de forma general
+	 * el filepath depende de que se este construyendo
+	 * estan en la parte de arriba 
+	 */
+	public static void WriteToFile(Object obj,String filepath) {
+		try {
+			Gson  gson = new Gson();
+			String JSON = gson.toJson(obj);
+			FileOutputStream fileOut = new FileOutputStream(filepath);
+			OutputStreamWriter myOutWriter = new OutputStreamWriter(fileOut);
+			myOutWriter.append(JSON);
+			myOutWriter.close();
+			fileOut.close();
+            System.out.println("The Object  was succesfully written to a file");
+ 
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+	
+	
+	// mirar si de esta forma se puede leer el objeto de tipo json, gracias.
+    public static Object ReadFromFile(String filepath) {
+   	 
+        try {
+ 
+            FileInputStream fileIn = new FileInputStream(filepath);
+            BufferedReader myReader = new BufferedReader(new InputStreamReader(fileIn));
+            String aDataRow ="";
+            String aBuffer ="";
+            while ((aDataRow=myReader.readLine())!=null) {
+            	aBuffer+=aDataRow;
+            }
+            myReader.close();
+            Gson  gson = new Gson();
+            Object obj =gson.fromJson(aBuffer,Object.class);
+            System.out.println("The Object has been read from the file");
+            return obj;
+ 
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+    
+    //esta es la forma de lectura que yo habia encontrado pero creo que es 
+    // mejor la que plantea Mateo
+    public static Object ReadFromFile1(String filepath) {
+   	 
+        try {
+ 
+            FileInputStream fileIn = new FileInputStream(filepath);
+            ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+ 
+            Object obj = objectIn.readObject();
+ 
+            System.out.println("The Object has been read from the file");
+            objectIn.close();
+            return obj;
+ 
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+	
+    
+    
+    // no borrar hasta comprobar que todo sirve
+    
+	/*public static void WritePerfilToFile(Object obj) {
 		try {
 			 
             FileOutputStream fileOut = new FileOutputStream(filepathPerfil);
@@ -113,7 +190,7 @@ public class Data {
             return null;
         }
     }
-    /*public static Perfil buscarUsuario (String username, int clave) {
+    public static Perfil buscarUsuario (String username, int clave) {
         //Perfil usuario = new Perfil(); //con el usuario y la clave, se debe buscar en la base de datos
         //un perfil existente, luego lo devuelven para que sea usado en la clase Login, por ejemplo.
         //return usuario;
