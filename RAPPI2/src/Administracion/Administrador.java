ecff1a274277a61d9dad6dc7ac2d31e1dde23ae2
Andrea <ptaborda@unal.edu.co>
2020-02-27 18:07:52 -0500

    update administrador

--- RAPPI2/src/Administracion/Administrador.java
+++ RAPPI2/src/Administracion/Administrador.java
@@ -34,7 +34,7 @@ public class Administrador extends Perfil {
 	 * @param salario El parametro salario define el salario que tendra el
 	 *                Administrador
 	 */
-	public Administrador(String nombre, int telefono, int comuna, int clave, String userName, long salario) {
+	public Administrador(String nombre, int telefono, int comuna, String clave, String userName, long salario) {
 		super(nombre, telefono, comuna, clave, userName);
 		this.salario = salario;
 	}// Cierre del constructor
@@ -47,7 +47,6 @@ public class Administrador extends Perfil {
 		this.salario = salario;
 	}
 
-<<<<<<< HEAD
 	public String toString() {
 		return this.getNombre() + ", " + this.getTelefono() + ", " + this.getComuna() + ", " + this.getClave() + ", "
 				+ this.getUserName() + ", " + this.getSalario();
@@ -67,7 +66,7 @@ public class Administrador extends Perfil {
 	 *                 Tendero para registrarse
 	 * @param salario  El parametro salario define el salario que tendra el Tendero
 	 */
-	public void crearTendero(String nombre, int telefono, int comuna, int clave, String userName, long salario) {
+	public void crearTendero(String nombre, int telefono, int comuna, String clave, String userName, long salario) {
 		Data.agreagarObjetoDataBasePerfil(new Tendero(nombre, telefono, comuna, clave, userName, salario));
 	}
 
@@ -91,89 +90,26 @@ public class Administrador extends Perfil {
 		while (true) {
 			if (posicion >= 0 && posicion <= 10) {
 				if (usuario instanceof Cliente) {
-					usuario = (Cliente) Main.usuario;
-					((Cliente) usuario).opciones.add(posicion);
+					while (true) {
+						if (posicion == 3 | posicion == 4 | posicion == 5 | posicion == 6 | posicion == 9
+								| posicion == 10) {
+							usuario = (Cliente) Main.usuario;
+							((Cliente) usuario).opciones.add(posicion);
+						} else {
+							System.out.println("Ingresa un numero valido");
+						}
+					}
 				} else if (usuario instanceof Tendero) {
-					usuario = (Tendero) Main.usuario;
-					((Tendero) usuario).opciones.add(posicion);
+					while (true) {
+						if (posicion == 7) {
+							usuario = (Tendero) Main.usuario;
+							((Tendero) usuario).opciones.add(posicion);
+						} else {
+							System.out.println("Ingrese un numero valido");
+						}
+					}
 				}
 			}
 		}
 	}
 }
-
-/**
- * Metodo que guarda los clientes que se han creado en el arreglo
- * {@link #clientes}
- * 
- * @param cliente El paramtetro cliente define el cliente que sera agregado a el
- *                arreglo
- */
-=======
-public class Administrador extends Perfil{
-    private long salario;
-    
-    /**
-     * Contructor para los objetos Administrador
-     * @see {@link Perfil#Perfil(String, int, int, int, String)}
-     * @param salario El parametro salario define el salario que tendra el Administrador
-     */
-    public Administrador(String nombre, int telefono, int comuna, String clave, String userName, long salario){
-        super(nombre, telefono, comuna, clave, userName);
-        this.salario = salario;
-    }//Cierre del constructor
-
-    public long getSalario(){
-        return this.salario;
-    }
-    
-    public void setSalario(long salario){
-        this.salario = salario;
-    }
-    
-    public String toString(){
-        return this.getNombre() + ", " + this.getTelefono() + ", " + this.getComuna() + ", " + this.getClave() + ", " + this.getUserName( )+ ", " + this.getSalario();
-    }
-    /**
-     * M�todo que crea un objeto de clase Tendero
-     * @param nombre El parametro nombre define el nombre que tendra el Tendero
-     * @param telefono El parametro telefono define el telefono que tendra el Tendero
-     * @param comuna El parametro comuna define la comuna en la que trabajara el Tendero
-     * @param clave El parametro clave define la clave que tendra el Tendero para registrarse
-     * @param userName El parametro userName define el userName que tendra el Tendero para registrarse
-     * @param salario El parametro salario define el salario que tendra el Tendero
-     */
-    public void crearTendero(String nombre, int telefono, int comuna, String clave, String userName, long salario) {
-    	Data.agreagarObjetoDataBasePerfil(new Tendero(nombre, telefono, comuna, clave, userName, salario));
-    }
-    
-    /**
-     * M�todo que crea un objeto de clase Restaurante
-     * @param nombre El parametro nombre define el nombre que tendra el Restaurante
-     * @param direccion El parametro direccion define la direccion nucleo del restaurante
-     * @param celular El parametro celular define el telefono celular que tendra el Restaurante
-     */
-    public void crearRestaurante(String nombre, String direccion, String celular) {
-    	Data.agreagarObjetoDataBaseRestaurante(new Restaurante(nombre,direccion,celular));
-    }
-    
-    public void ListarOpciones(String userName) {
-    	Perfil usuario = Data.buscarUsuario(userName);
-    	
-    	if (usuario instanceof Cliente) {
-    		
-    	}
-    	else if (usuario instanceof Tendero) {
-    		
-    	}
-    	else if (usuario instanceof Administrador) {
-    		
-    	}
-    }
-    /**
-     * Metodo que guarda los clientes que se han creado  en el arreglo {@link #clientes}
-     * @param cliente El paramtetro cliente define el cliente que sera agregado a el arreglo
-     */
-    
-}
->>>>>>> 8f24451b1a2c5bd757dc957ceb86640cb8331fad
