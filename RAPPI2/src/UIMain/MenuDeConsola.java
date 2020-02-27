669bd72807fe52de07be9b6961693baa85529db4
Andrea <ptaborda@unal.edu.co>
2020-02-27 14:03:26 -0500

    Update MenuDeConsola.java

--- RAPPI2/src/UIMain/MenuDeConsola.java
+++ RAPPI2/src/UIMain/MenuDeConsola.java
@@ -1,5 +1,6 @@
 package UIMain;
 
+import java.util.Scanner;
 /**
  * Clase MenuDeConsola
  * tiene como finalidad desplegar el menu de consola que se requiere para este trabajo
@@ -8,17 +9,35 @@ package UIMain;
  * Estructuras revelantes
  * ------------------
  * 
- * @author:
+ * @author:Guillermo Toloza
  * @version:
  */
 
 public class MenuDeConsola{  //Se desplega cada vez que se vaya a crear un menu diferente. El tamano
     //varia dependiendo de las opciones de cada menu.
 
-    public void MenuDeConsola(int size){
-
-        OpcionDeMenu[] menu = new OpcionDeMenu[size];
-        
+    public MenuDeConsola(int size){
+        OpcionDeMenu[] Menu = new OpcionDeMenu[size];        
+    }
+    public void AnadirOpcion(OpcionDeMenu OpcionMenu) {
+    }
+    
+    public String LanzarMenu() {
+    	return 
+    }
+    static Scanner teclado = new Scanner(System.in);
+    static long readLong() {
+    	return teclado.nextLong();
+    }
+    static String readString() {
+    	teclado.nextLine();
+    	return teclado.nextLine();
     }
 
+
+    public static void main (String args[]) {
+    	MenuDeConsola MenuInicio = new MenuDeConsola(3); //Size: , registro, cliente, cliente sin registrar
+    	
+	
+    }
 }
