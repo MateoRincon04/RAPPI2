e007c780f3bdb31742e6fac8b4ac2117eac6e520
Santiago Tamayo <stamayol@unal.edu.co>
2020-02-29 23:26:06 -0500

    faltaba una cosita

--- RAPPI2/src/UIMain/Main.java
+++ RAPPI2/src/UIMain/Main.java
@@ -5,6 +5,7 @@ import Oferta.*;
 import Interaccion.*;
 
 import java.io.EOFException;
+import java.io.IOException;
 import java.util.Scanner;
 
 import Administracion.*;
@@ -14,8 +15,9 @@ public class Main {
 	public static Perfil usuario;
 	public static Scanner user = new Scanner(System.in);
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		Data.CargarOpciones();
+		Data.cargarFileDataBasePerfil();
 		MenuDeConsola menuPrrrrr = new MenuDeConsola();
 		menuPrrrrr.lanzarMenu();
 		user.close();
