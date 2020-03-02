7337a16270c3015828c875f2e27504809f7f3de4
GuilleTG00 <gtoloza@unal.edu.co>
2020-03-01 23:56:18 -0500

    commit commit commit commit commit
    commitcommitcommitcommit

---
+++ RAPPI2/src/UIMain/CrearAdministrador.java
@@ -0,0 +1,28 @@
+package UIMain;
+
+import Administracion.Administrador;
+
+public class CrearAdministrador implements OpcionDeMenu {
+	public void ejecutar() {
+		System.out.println("Usted ingresará un nuevo Administrador en el sistema.");
+		System.out.println("Ingrese el nombre: ");
+		String nombre = Main.user.next();
+		System.out.println("Ingrese la clave: ");
+		String clave = Main.user.next();
+		System.out.println("Ingrese el username: ");
+		String username = Main.user.next();
+		System.out.println("Ingrese el numero de telefono: ");
+		int telefono = Main.user.nextInt();
+		System.out.println("Ingrese el salario: ");
+		int salario = Main.user.nextInt();
+		System.out.println("Ingrese la comuna: ");
+		int comuna = Main.user.nextInt();
+		Administrador admin = (Administrador) Main.usuario;
+		boolean valor = admin.crearAdministrador(nombre, telefono, comuna, clave, username, salario);
+		if (!valor) {
+			ejecutar();
+
+		}
+	}
+
+}
