b979969eb262c4a437cb4c4f071213e5baa08f79
GuilleTG00 <gtoloza@unal.edu.co>
2020-03-15 21:15:59 -0500

    commit commit commit commit commit commit commit commit

--- RAPPI2/src/UIMain/Tendero/MenuTendero.java
+++ RAPPI2/src/UIMain/Tendero/MenuTendero.java
@@ -1,8 +1,8 @@
-package UIMain.Cliente;
+package UIMain.Tendero;
 
 import BaseDatos.Data;
-import UIMain.Main;
 import gestorAplicacion.Interaccion.Cliente;
+import gestorAplicacion.Interaccion.Tendero;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -16,7 +16,7 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class MenuCliente extends Application {
+public class MenuTendero extends Application {
 	Button archivos;
 	Button procesosYConsultas;
 	Button ayuda;
@@ -25,24 +25,23 @@ public class MenuCliente extends Application {
 	BorderPane panelcin;
 
 	public void start(Stage myStage) {
-		// myStage.setTitle("Usuario: " + Main.usuario.getNombre());
-			panel = new BorderPane();
-			FlowPane parteSuperior = new FlowPane();
-			archivos = new Button("Archivos");
-			HanderClass handler = new HanderClass();
-			procesosYConsultas = new Button("Procesos y Consultas");
-			ayuda = new Button("Ayuda");
-			archivos.setOnAction(handler);
-			procesosYConsultas.setOnAction(handler);
-			ayuda.setOnAction(handler);
-			parteSuperior.getChildren().add(archivos);
-			parteSuperior.getChildren().add(procesosYConsultas);
-			parteSuperior.getChildren().add(ayuda);
-			parteSuperior.setHgap(5);
-			panel.setTop(parteSuperior);
-			Scene scene = new Scene(panel, 1200, 600);
-			myStage.setScene(scene);
-			myStage.show();
+		panel = new BorderPane();
+		FlowPane parteSuperior = new FlowPane();
+		archivos = new Button("Archivos");
+		HanderClass handler = new HanderClass();
+		procesosYConsultas = new Button("Procesos y Consultas");
+		ayuda = new Button("Ayuda");
+		archivos.setOnAction(handler);
+		procesosYConsultas.setOnAction(handler);
+		ayuda.setOnAction(handler);
+		parteSuperior.getChildren().add(archivos);
+		parteSuperior.getChildren().add(procesosYConsultas);
+		parteSuperior.getChildren().add(ayuda);
+		parteSuperior.setHgap(5);
+		panel.setTop(parteSuperior);
+		Scene scene = new Scene(panel, 1200, 600);
+		myStage.setScene(scene);
+		myStage.show();
 	}
 
 	public static void main(String[] args) {
@@ -54,28 +53,28 @@ public class MenuCliente extends Application {
 			Object control = e.getSource();
 			if (control instanceof Button) {
 				if (control.equals(archivos)) {
-					Cliente cliente = Data.buscarCliente("santiago");
+					Tendero tendero = Data.buscarTendero("p");
 					BorderPane panelcito = new BorderPane();
 					GridPane nuevoPanel = new GridPane();
-					Label saludo = new Label("Estos son los datos del Cliente: ");
+					Label saludo = new Label("Estos son los datos del Tendero	: ");
 					panelcito.setTop(saludo);
 					panelcito.setAlignment(saludo, Pos.CENTER);
 					Label nombre = new Label(" Nombre: ");
-					Label valorNombre = new Label(" " + cliente.getNombre() + " ");
+					Label valorNombre = new Label(" " + tendero.getNombre() + " ");
 					Label clave = new Label(" clave: ");
-					Label valorClave = new Label(" " + cliente.getClave() + " ");
+					Label valorClave = new Label(" " + tendero.getClave() + " ");
 					Label username = new Label(" username: ");
-					Label valorUsername = new Label(" " + cliente.getUserName() + " ");
+					Label valorUsername = new Label(" " + tendero.getUserName() + " ");
 					Label telefono = new Label(" Telefono: ");
-					Label valorTelefono = new Label(" " + String.valueOf(cliente.getTelefono()) + " ");
+					Label valorTelefono = new Label(" " + String.valueOf(tendero.getTelefono()) + " ");
 					Label comuna = new Label(" Comuna: ");
-					Label valorComuna = new Label(" " + String.valueOf(cliente.getComuna()) + " ");
-					Label metodoDePago = new Label(" Metodo de Pago: ");
-					Label valorMetodoDePago = new Label(" " + cliente.getMetodoDePago() + " ");
-					Label saldo = new Label(" Saldo: ");
-					Label valorSaldo = new Label(" " + String.valueOf(cliente.getSaldo()) + " ");
-					Label direccion = new Label(" Direccion: ");
-					Label valorDireccion = new Label(" " + cliente.getDireccion() + " ");
+					Label valorComuna = new Label(" " + String.valueOf(tendero.getComuna()) + " ");
+					Label estaDisponible = new Label(" Esta disponible: ");
+					Label valorEstaDisponible = new Label(" " + tendero.getEstaDisponible() + " ");
+					Label entregados = new Label(" Pedidos Entregados: ");
+					Label valorEntregados = new Label(" " + String.valueOf(tendero.getEntregados() + " "));
+					Label salario = new Label(" Salario: ");
+					Label valorSalario = new Label(" " + tendero.getSalario() + " ");
 					nuevoPanel.add(nombre, 0, 0);
 					nuevoPanel.add(valorNombre, 1, 0);
 					nuevoPanel.add(username, 0, 1);
@@ -86,12 +85,12 @@ public class MenuCliente extends Application {
 					nuevoPanel.add(valorTelefono, 1, 3);
 					nuevoPanel.add(comuna, 0, 4);
 					nuevoPanel.add(valorComuna, 1, 4);
-					nuevoPanel.add(metodoDePago, 0, 5);
-					nuevoPanel.add(valorMetodoDePago, 1, 5);
-					nuevoPanel.add(saldo, 0, 6);
-					nuevoPanel.add(valorSaldo, 1, 6);
-					nuevoPanel.add(direccion, 0, 7);
-					nuevoPanel.add(valorDireccion, 1, 7);
+					nuevoPanel.add(estaDisponible, 0, 5);
+					nuevoPanel.add(valorEstaDisponible, 1, 5);
+					nuevoPanel.add(entregados, 0, 6);
+					nuevoPanel.add(valorEntregados, 1, 6);
+					nuevoPanel.add(salario, 0, 7);
+					nuevoPanel.add(valorSalario, 1, 7);
 					nuevoPanel.setStyle("-fx-background-color: white; -fx-grid-lines-visible: true");
 					panelcito.setCenter(nuevoPanel);
 					nuevoPanel.setAlignment(Pos.CENTER);
