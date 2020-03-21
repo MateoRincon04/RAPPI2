a8d58114ad6875f2d03398ab93440e014237e2c5
Santiago Tamayo <stamayol@unal.edu.co>
2020-03-21 14:46:41 -0500

    correccion de errores

--- RAPPI2/src/UIMain/FieldPanel.java
+++ RAPPI2/src/UIMain/FieldPanel.java
@@ -1,10 +1,17 @@
 package UIMain;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class FieldPanel extends Pane{
 	String tituloCriterios;
@@ -15,14 +22,17 @@ public class FieldPanel extends Pane{
 	GridPane root;
 	public FieldPanel(String tituloCriterios,String[] criterios, String tituloValores, String[] valores,boolean[] habilitado) {
 		root = new GridPane();
+		root.setPadding(new Insets(10,100,10,100));
+		root.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID,CornerRadii.EMPTY,BorderWidths.DEFAULT)));
 		root.setHgap(5);
 		root.setVgap(5);
-		root.setAlignment(Pos.CENTER);
-		TextField tc = new TextField(tituloCriterios);
-		tc.setEditable(false);
+		Label tc = new Label(tituloCriterios);
+		tc.setPadding(new Insets(2,2,2,2));
+		tc.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID,CornerRadii.EMPTY,BorderWidths.DEFAULT)));
 		root.add(tc, 0, 0);
-		TextField tv =new TextField(tituloValores);
-		tv.setEditable(false);
+		Label tv =new Label(tituloValores);
+		tv.setPadding(new Insets(2,2,2,2));
+		tv.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID,CornerRadii.EMPTY,BorderWidths.DEFAULT)));
 		root.add(tv, 1, 0);
 		for(int i = 0; i<criterios.length; i++) {
 			root.add(new Label(criterios[i]), 0, i+1);
@@ -33,5 +43,6 @@ public class FieldPanel extends Pane{
 			root.add(taux, 1, i+1);
 		}
 		this.getChildren().add(root);
+		//this.setAlignment(Pos.CENTER);
 	}
 }
