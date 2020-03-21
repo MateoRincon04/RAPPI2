f2ac063b9f0fb19a5ed8f29a15849746b7b02b04
MateoRincon04 <>
2020-03-21 14:12:05 -0500

    restau

--- RAPPI2/src/UIMain/Restaurante/RestauranteEscena.java
+++ RAPPI2/src/UIMain/Restaurante/RestauranteEscena.java
@@ -7,6 +7,8 @@ import javax.swing.JComboBox;
 
 import BaseDatos.Data;
 import UIMain.Main;
+import gestorAplicacion.Administracion.Administrador;
+import gestorAplicacion.Oferta.Restaurante;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -19,10 +21,15 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
@@ -32,273 +39,179 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class RestauranteEscena extends Application {
-
-	Stage window;
-	Button Archivo;
-	Button ProcYConsul;
-	Button edit;
-	Button Ayuda;
-	ComboBox cbx1;
-	Scene scene;
-	FlowPane root;
-	BorderPane bord;
-	GridPane grid;
-	static BorderPane bp;
-	Alert a;
-	Label lbl = new Label();
-	FlowPane fl;
-	GridPane ayuda;
-	String dondeAndo = "";
-	Scene sceneEdit;
-	BorderPane proc;
-	SwingNode sw = new SwingNode();
-	JComboBox<String> comboBox;
+	Scene sceneRes;
+	GridPane gp;
+	Restaurante usuario = Main.usuarioRestaurante;
 
 	public void start(Stage stage) {
 
-		window = stage;
-		Archivo = new Button("Archivo");
-		ProcYConsul = new Button("Procesos y Consultas");
-		Ayuda = new Button("Ayuda");
+		// Manejo de la barra de menú de la vantana
+		MenuBar barraMenu = new MenuBar();
 
-		Handler handler = new Handler();
-		Archivo.setOnAction(handler);
-		ProcYConsul.setOnAction(handler);
-		Ayuda.setOnAction(handler);
-		// FlowPane
-		root = new FlowPane();
-		root.setVgap(8);
-		root.setHgap(4);
-		root.getChildren().add(Archivo);
-		root.getChildren().add(ProcYConsul);
-		root.getChildren().add(Ayuda);
+		// Definición de menú
+		Menu menu = new Menu("Archivo");
+		Menu menu1 = new Menu("Procesos y consultas");
+		Menu menu2 = new Menu("Ayuda");
+		barraMenu.getMenus().addAll(menu, menu1, menu2);
 
-		// BorderPane
-		bord = new BorderPane();
-		bord.setTop(root);
-		scene = new Scene(bord, 1200, 600);
+		// se adiciona los elementos de archivo
+		MenuItem mi1 = new MenuItem("Usuario");
+		MenuItem mi2 = new MenuItem("Salir");
+		SeparatorMenuItem separator = new SeparatorMenuItem();
+		menu.getItems().addAll(mi1, separator, mi2);
 
-		window.setTitle("Usuario: " + Main.usuarioRestaurante.getNombre()); // + Main.usuarioRestaurante.getNombre()
-		window.setScene(scene);
-		window.show();
+		// se adiciona los elementos de Procesos y consultas
+		for (int i = 14; i < 22; i++) {
+			menu1.getItems().add(new MenuItem(Data.getOpciones().get(i).toString()));
+			menu1.getItems().add(new SeparatorMenuItem());
+		}
+		menu1.getItems().add(new MenuItem(Data.getOpciones().get(29).toString()));
+		menu1.getItems().add(new SeparatorMenuItem());
 
-	}
+		// se adiciona los elementos de ayuda
+		MenuItem mi3 = new MenuItem("Acerca de");
+		menu2.getItems().add(mi3);
 
-	class Handler implements EventHandler<ActionEvent> {
-		@Override
-		public void handle(ActionEvent e) {
-			Object control = e.getSource();
-			if (control instanceof Button) {
-				if (control.equals(Archivo)) {
-					if (dondeAndo.equals("Procesos y Consultas")) {
-						//bord.getChildren().remove();
+		// se adicion la barra de menu al pane1
+		BorderPane root = new BorderPane();
+		root.setTop(barraMenu);
+		// creación de la Escena
+		sceneRes = new Scene(root, 600, 600);
 
-					} else if (dondeAndo.equals("Ayuda")) {
-						//bord.getChildren().remove();
-					}
-
-					grid = new GridPane();
-					Label lab1 = new Label("Nombre: ");
-					Label lab2 = new Label("Direcciones: ");
-					Label lab3 = new Label("Celular: ");
-					Label lab4 = new Label("Clave: ");
-					Label ins = new Label("separe las direcciones por ,");
-					String aux = "";
-					Label lab1_1 = new Label(Main.usuarioRestaurante.getNombre());
-					for (int i = 0; i < Main.usuarioRestaurante.getDireccion().size(); i++) {
-						String la = Main.usuarioRestaurante.getDireccion().get(i);
-						if (i == 0) {
-							aux = la;
-						} else {
-							aux = aux + ", " + la;
-						}
-
-					}
-					Label lab1_2 = new Label(aux);
-					Label lab1_3 = new Label(Main.usuarioRestaurante.getCelular());
-					Label lab1_4 = new Label(Main.usuarioRestaurante.getClave());
-
-					TextField tx_1 = new TextField();
-					tx_1.setText(Main.usuarioRestaurante.getNombre());
-					tx_1.setEditable(false);
-					TextField tx_2 = new TextField();
-					tx_2.setText(aux);
-					TextField tx_3 = new TextField();
-					tx_3.setText(Main.usuarioRestaurante.getCelular());
-					TextField tx_4 = new TextField();
-					tx_4.setText(Main.usuarioRestaurante.getClave());
-					tx_4.setEditable(false);
-
-					grid = new GridPane();
-					grid.add(new Label("Datos del restaurante: "), 0, 0);
-					grid.add(lab1, 0, 1);
-					grid.add(lab2, 0, 2);
-					grid.add(lab3, 0, 3);
-					grid.add(lab4, 0, 4);
-					grid.add(lab1_1, 1, 1);
-					grid.add(lab1_2, 1, 2);
-					grid.add(lab1_3, 1, 3);
-					grid.add(lab1_4, 1, 4);
-
-					grid.setPadding(new Insets(10, 10, 10, 10));
-					grid.setVgap(5);
-					grid.setHgap(5);
-					grid.setAlignment(Pos.CENTER);
-					bord.setCenter(grid);
-
-					Button can = new Button("Cancelar");
-
-					Button acep = new Button("Aceptar cambios");
-					acep.setOnAction(new EventHandler<ActionEvent>() {
-
-						@Override
-						public void handle(ActionEvent event) {
-							a = new Alert(AlertType.CONFIRMATION);
-							a.setTitle("Confirmacion");
-							a.setContentText("Confirmacion de los cambio realizados");
-							Optional<ButtonType> result = a.showAndWait();
-							if (result.get() == ButtonType.OK) {
-								Main.usuarioRestaurante.setCelular(tx_3.getText());
-								Main.usuarioRestaurante.setDireccion(tx_2.getText());
-								bord.getChildren().remove(grid);
-								grid.getChildren().remove(tx_1);
-								grid.getChildren().remove(tx_2);
-								grid.getChildren().remove(tx_3);
-								grid.getChildren().remove(tx_4);
-								grid.getChildren().remove(acep);
-								grid.getChildren().remove(ins);
-								grid.getChildren().remove(can);
-								grid.add(lab1_1, 1, 1);
-								grid.add(lab1_2, 1, 2);
-								grid.add(lab1_3, 1, 3);
-								grid.add(lab1_4, 1, 4);
-								bord.setCenter(grid);
-
-							} else {
-								bord.getChildren().remove(grid);
-								grid.getChildren().remove(tx_1);
-								grid.getChildren().remove(tx_2);
-								grid.getChildren().remove(tx_3);
-								grid.getChildren().remove(tx_4);
-								grid.getChildren().remove(acep);
-								grid.getChildren().remove(ins);
-								grid.getChildren().remove(can);
-								grid.add(lab1_1, 1, 1);
-								grid.add(lab1_2, 1, 2);
-								grid.add(lab1_3, 1, 3);
-								grid.add(lab1_4, 1, 4);
-								bord.setCenter(grid);
-							}
-
-						}
-					});
-
-					can.setOnAction(new EventHandler<ActionEvent>() {
-
-						@Override
-						public void handle(ActionEvent event) {
-							bord.getChildren().remove(grid);
-							grid.getChildren().remove(tx_1);
-							grid.getChildren().remove(tx_2);
-							grid.getChildren().remove(tx_3);
-							grid.getChildren().remove(tx_4);
-							grid.getChildren().remove(acep);
-							grid.getChildren().remove(ins);
-							grid.getChildren().remove(can);
-							grid.add(lab1_1, 1, 1);
-							grid.add(lab1_2, 1, 2);
-							grid.add(lab1_3, 1, 3);
-							grid.add(lab1_4, 1, 4);
-							bord.setCenter(grid);
-						}
-
-					});
-
-					edit = new Button("Editar");
-					edit.setOnAction(new EventHandler<ActionEvent>() {
-
-						@Override
-						public void handle(ActionEvent event) {
-							bord.getChildren().remove(grid);
-							grid.getChildren().remove(lab1_1);
-							grid.getChildren().remove(lab1_2);
-							grid.getChildren().remove(lab1_3);
-							grid.getChildren().remove(lab1_4);
-							grid.add(tx_1, 1, 1);
-							grid.add(tx_2, 1, 2);
-							grid.add(tx_3, 1, 3);
-							grid.add(tx_4, 1, 4);
-							grid.add(acep, 1, 5);
-							grid.add(ins, 2, 2);
-							grid.add(can, 2, 5);
-							bord.setCenter(grid);
-
-						}
-					});
-
-					dondeAndo = "Archivo";
-					bord.setBottom(edit);
-					window.setScene(scene);
-					window.show();
-
-				} else if (control.equals(ProcYConsul)) {
-					if (dondeAndo.equals("Archivo")) {
-						bord.getChildren().remove(edit);
-						bord.getChildren().remove(grid);
-					} else if (dondeAndo.equals("Ayuda")) {
-
-					}
-
-					bp = new BorderPane();
-
-					ArrayList<String> opciones = new ArrayList<String>();
-					for (int i = 0; i < Main.usuarioRestaurante.getOpciones().size(); i++) {
-						opciones.add(Data.getOpciones().get(Main.usuarioRestaurante.getOpciones().get(i)).toString());
-					}
-
-					cbx1 = new ComboBox(FXCollections.observableArrayList(opciones));
-					cbx1.setPromptText("Opciones");
-					cbx1.valueProperty().addListener(change);
-
-					bp.setTop(cbx1);
-
-					bord.setCenter(bp);
-					dondeAndo = "Procesos y Consultas";
-					window.setScene(scene);
-					window.show();
-
-				} else if (control.equals(Ayuda)) {
-					ayuda = new GridPane();
-					Label ay = new Label("Ayuda para el uso de tu menu, escriba su duda: ");
-					Label nom = new Label("Nombres de los autores: ");
-					Label espacio = new Label(" ");
-					Label nombres = new Label("Santiago Tamayo, Paula Andrea Taborda, Guillermo Toloza, Mateo Rincón");
-					TextField tf = new TextField();
-					ayuda.add(nom, 0, 0);
-					ayuda.add(nombres, 0, 1);
-					ayuda.add(espacio, 0, 2);
-					ayuda.add(ay, 0, 3);
-					ayuda.add(tf, 0, 4);
-
+		// programa para Archivo SALIR
+		mi2.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				Alert conf = new Alert(AlertType.CONFIRMATION);
+				conf.setTitle("Confirmacion de salida");
+				conf.setContentText("¿Seguro que desea salir?");
+				Optional<ButtonType> result = conf.showAndWait();
+				if (result.get() == ButtonType.OK) {
+					Data.getOpciones().get(29).ejecutar();
+				} else {
+					// nada
 				}
 			}
-		}
+		});
+
+		// programa para Archivo USUARIO
+		mi1.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				// informacion del restaurante por medio de el FieldPanel
+				String tituloCriterios = "Datos del Restaurante";
+				String[] criterios = new String[] { "Nombre", "Direcciones", "Celular", "Menu", "Clave" };
+				String tituloValores = "";
+				String aux = "";
+				for (int in = 0; in < Main.usuarioRestaurante.getDireccion().size(); in++) {
+					if (in == 0) {
+						aux = usuario.getDireccion().get(0);
+
+					} else {
+						aux = aux + ", " + usuario.getDireccion().get(in);
+					}
+
+				}
+				String[] valores = new String[] { usuario.getNombre(), aux, usuario.getCelular(), usuario.getMenu(),
+						usuario.getClave() };
+				boolean[] habilitado = new boolean[] { false, true, true, true, false };
+
+				// FieldPanel fp = new FieldPanel(tituloCriterios,criterios,tituloValores,valores,habilitado);
+
+				// se crea la ventana con la informacion basica del Administrador
+				gp = new GridPane();
+				// crear los nodos del grid
+				Label lb1 = new Label("Nombre");
+				TextField t1 = new TextField(usuario.getNombre());
+				t1.setEditable(false);
+				Label lb2 = new Label("UserName");
+				TextField t2 = new TextField(usuario.getUserName());
+				t2.setEditable(false);
+				Label lb3 = new Label("Clave");
+				String claveAux = "";
+				for (int i = 0; i < usuario.getClave().length(); i++) {
+					claveAux = claveAux + "*";
+				}
+				TextField t3 = new TextField(claveAux);
+				t3.setEditable(false);
+				t3.setOnMouseEntered(new EventHandler<MouseEvent>() {
+					public void handle(MouseEvent mouseEvent) {
+						t3.setText(usuario.getClave());
+					}
+				});
+				t3.setOnMouseExited(new EventHandler<MouseEvent>() {
+					public void handle(MouseEvent mouseEvent) {
+						String claveAux = "";
+						for (int i = 0; i < usuario.getClave().length(); i++) {
+							claveAux = claveAux + "*";
+						}
+						t3.setText(claveAux);
+					}
+				});
+				Label lb4 = new Label("Telefono");
+				TextField t4 = new TextField(String.valueOf(usuario.getTelefono()));
+				t4.setEditable(false);
+				Label lb5 = new Label("Comuna");
+				TextField t5 = new TextField(String.valueOf(usuario.getComuna()));
+				t5.setEditable(false);
+				Label lb6 = new Label("Salario");
+				TextField t6 = new TextField(String.valueOf(usuario.getSalario()));
+				t6.setEditable(false);
+				// especificaciones del grip
+				gp.setHgap(5);
+				gp.setVgap(5);
+				gp.setAlignment(Pos.CENTER);
+				// agregar los nodos al grip
+				gp.add(lb1, 0, 0);
+				gp.add(t1, 1, 0);
+				gp.add(lb2, 0, 1);
+				gp.add(t2, 1, 1);
+				gp.add(lb3, 0, 2);
+				gp.add(t3, 1, 2);
+				gp.add(lb4, 0, 3);
+				gp.add(t4, 1, 3);
+				gp.add(lb5, 0, 4);
+				gp.add(t5, 1, 4);
+				gp.add(lb6, 0, 5);
+				gp.add(t6, 1, 5);
+				gp.setAlignment(Pos.CENTER);
+				root.setCenter(gp);
+			}
+		});
+
+		// Programa para las funcionalidades
+		// En cuales direcciones
+		menu1.getItems().get(0).setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				String tituloCriterios = "Datos necesarios para la operacion";
+				String[] criterios = new String[] { "Nombre", "Direcciones", "Celular", "Menu", "Clave" };
+				String tituloValores = "";
+				String aux = "";
+				for (int in = 0; in < Main.usuarioRestaurante.getDireccion().size(); in++) {
+					if (in == 0) {
+						aux = usuario.getDireccion().get(0);
+
+					} else {
+						aux = aux + ", " + usuario.getDireccion().get(in);
+					}
+
+				}
+				String[] valores = new String[] { usuario.getNombre(), aux, usuario.getCelular(), usuario.getMenu(),
+						usuario.getClave() };
+				boolean[] habilitado = new boolean[] { false, true, true, true, false };
+
+				// FieldPanel fp = new FieldPanel(tituloCriterios,criterios,tituloValores,valores,habilitado);
+
+			}
+		});
+
+		// Display sceneRes at first
+		stage.setScene(sceneRes);
+		stage.setTitle("Usuario: " + usuario.getNombre());
+		stage.show();
 
 	}
-
-	ChangeListener<String> change = new ChangeListener<String>() {
-
-		public void changed(ObservableValue ov, String t, String t1) {
-			if (t1.equals("Agregar Direccion Restaurante")) {
-				bp.setCenter(((AgregarDireccionRest) Data.getOpciones().get(14)).ejecutar(t1));
-			}
-
-		};
-	};
 
 	public static void main(String[] args) {
 		Data.CargarOpciones();
 		launch(args);
 	}
-
 }
