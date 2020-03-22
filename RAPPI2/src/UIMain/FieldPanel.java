86210e3991cf262ac44746a1b7cddf657bcfa069
Santiago Tamayo <stamayol@unal.edu.co>
2020-03-22 11:01:56 -0500

    m

--- RAPPI2/src/UIMain/FieldPanel.java
+++ RAPPI2/src/UIMain/FieldPanel.java
@@ -1,6 +1,7 @@
 package UIMain;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Border;
@@ -24,18 +25,22 @@ public class FieldPanel extends Pane{
 		entradas = new TextField[criterios.length];
 		root = new GridPane();
 		root.setPadding(new Insets(10,100,10,100));
-		root.setBorder(new Border(new BorderStroke(Color.BLACK,BorderStrokeStyle.SOLID,CornerRadii.EMPTY,BorderWidths.DEFAULT)));
-		root.setHgap(5);
+		root.setBorder(new Border(new BorderStroke(Color.GREY,BorderStrokeStyle.SOLID,CornerRadii.EMPTY,BorderWidths.DEFAULT)));
+		root.setHgap(40);
 		root.setVgap(5);
 		Label tc = new Label(tituloCriterios);
 		tc.setTextFill(Color.BLUE);
 		tc.setPadding(new Insets(2,2,2,2));
 		tc.setBorder(new Border(new BorderStroke(Color.BLUE,BorderStrokeStyle.SOLID,CornerRadii.EMPTY,BorderWidths.DEFAULT)));
+		tc.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		tc.setAlignment(Pos.CENTER);
 		root.add(tc, 0, 0);
 		Label tv =new Label(tituloValores);
 		tv.setTextFill(Color.BLUE);
 		tv.setPadding(new Insets(2,2,2,2));
 		tv.setBorder(new Border(new BorderStroke(Color.BLUE,BorderStrokeStyle.SOLID,CornerRadii.EMPTY,BorderWidths.DEFAULT)));
+		tv.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		tv.setAlignment(Pos.CENTER);
 		root.add(tv, 1, 0);
 		for(int i = 0; i<criterios.length; i++) {
 			root.add(new Label(criterios[i]), 0, i+1);
