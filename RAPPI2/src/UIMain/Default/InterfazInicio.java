127415ad398dddec15a132c6422c1e90ce588895
Andrea <ptaborda@unal.edu.co>
2020-03-25 16:47:59 -0500

    Update inicio interfaz

--- RAPPI2/src/UIMain/Default/InterfazInicio.java
+++ RAPPI2/src/UIMain/Default/InterfazInicio.java
@@ -1,40 +1,131 @@
 package UIMain.Default;
 
 import javafx.application.*;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Border;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class InterfazInicio extends Application{
+	GridPane P1 = new GridPane();
+	BorderPane P2 = new BorderPane();
+	BorderPane P3 = new BorderPane();
+	FlowPane f1 = new FlowPane();
+	FlowPane f2 = new FlowPane();
+	Button login = new Button("Log In");
+	Button signup = new Button("Sign Up");
+	Label autores = new Label("AUTORES");
+	Image imagen1 = new Image(getClass().getResourceAsStream("UIMain.Default.Images/tender2.png"));
+	ImageView imagenView1 = new ImageView(imagen1);
+	Label imagenlabel1 = new Label("",imagenView1);
+	Label generico = new Label("");
+	Label mateo = new Label("Mateo Rincón Arias\r\n" + 
+			"Estudiante\r\n" + 
+			"Bachiller cursando el cuarto semestre de educacion superior\r\n" + 
+			"Informacion de contacto:\r\n" + 
+			"Email:\r\n" + 
+			"    rincon.mateo04@gmail.com\r\n" + 
+			"Telefono:\r\n" + 
+			"    (+57) 321 831 4047\r\n" + 
+			"Fecha de nacimiento:\r\n" + 
+			"    1999-03-27\r\n" + 
+			"Idiomas:\r\n" + 
+			"    Ingles\r\n" + 
+			"        Nivel avanzado\r\n" + 
+			"Educacion:\r\n" + 
+			"    Estudiante de Ingenieria de Sistemas e Informatica\r\n" + 
+			"        Universidad Nacional de Colombia, sede Medellin\r\n" + 
+			"    Estudiante de Ingenieria de Matematica\r\n" + 
+			"        Universidad Nacional de Colombia, sede Medellin\r\n" + 
+			"    Bachiller\r\n" + 
+			"    New Germany Rural High School. New Germany, Canada\r\n" + 
+			"    2017\r\n" + 
+			"    Bachiller\r\n" + 
+			"    San Jose de las Vegas. Medellin, Colombia\r\n" + 
+			"    2016");
+	Label santiago = new Label("Santiago Tamayo Lopez\r\n" + 
+			"Estudiante \r\n" + 
+			"\r\n" + 
+			"Bachiller cursando el cuarto semestre de educacion superior\r\n" + 
+			"\r\n" + 
+			"Informacion de contacto:\r\n" + 
+			"Email:\r\n" + 
+			"	stamayol@unal.edu.co\r\n" + 
+			"Telefono:\r\n" + 
+			"	(+57) 302 273 2242\r\n" + 
+			"Fecha de nacimiento:\r\n" + 
+			"	2001-04-21\r\n" + 
+			"Idiomas:\r\n" + 
+			"	Ingles\r\n" + 
+			"		 Nivel Avanzado\r\n" + 
+			"Educacion:\r\n" + 
+			"	Estudiante de Ingenieria de Sistemas e Informatica\r\n" + 
+			"		Universidad Nacional de Colombia, Sede Medellin\r\n" + 
+			"	Bachiller\r\n" + 
+			"		Colegio Parroquial Emaus. Medellin, Colombia 2017");
+	Label guille = new Label("Guillermo Toloza Guzmán\r\n" + 
+			"Estudiante UNAL\r\n" + 
+			"Actualmente cursando 3er semestre en la Universidad Nacional de Colombia\r\n" + 
+			"Datos de contacto:\r\n" + 
+			"Gmail:\r\n" + 
+			"gtoloza@unal.edu.co\r\n" + 
+			"Fecha de Nacimiento:\r\n" + 
+			"02-06-2000\r\n" + 
+			"Lugar de Nacimiento:\r\n" + 
+			"San José de Cúcuta, Norte de Santander\r\n" + 
+			"Colombia\r\n" + 
+			"Manejo de Idiomas:\r\n" + 
+			"Ingles: Nivel Avanzado\r\n" + 
+			"Estudios:\r\n" + 
+			"Estudios Superiores: \r\n" + 
+			"Estudiante de Ingeniería de Sistemas e Informática, UNAL Sede Medellín\r\n" + 
+			"Bachiller:\r\n" + 
+			"Colegio Gimnasio Domingo Savio, san José De Cúcuta, Norte de Santander\r\n" + 
+			"Año 2017.");
+	Label andre = new Label("Paula Andrea Taborda Jaramillo\r\n" + 
+			"Estudiante \r\n" + 
+			"Bachiller cursando el cuarto semestre de educacion superior\r\n" + 
+			"Informacion de contacto:\r\n" + 
+			"Email:\r\n" + 
+			"	ptaborda@unal.edu.co\r\n" + 
+			"Telefono:\r\n" + 
+			"	(+57) 320 752 2464\r\n" + 
+			"Fecha de nacimiento:\r\n" + 
+			"	2000-05-02\r\n" + 
+			"Idiomas:\r\n" + 
+			"	Ingles\r\n" + 
+			"		 Nivel Avanzado\r\n" + 
+			"Educacion:\r\n" + 
+			"	Estudiante de Ingenieria de Sistemas e Informatica\r\n" + 
+			"		Universidad Nacional de Colombia, Sede Medellin\r\n" + 
+			"	Bachiller\r\n" + 
+			"		Diego Echavarria Misas. Medellin, Colombia 2017");
+	
 	
 	public void start(Stage MyStage) throws Exception{
-		
+	
 		MyStage.setTitle("RAPPI2");
 		
-		GridPane P1 = new GridPane();
-		GridPane P2 = new GridPane();
-		BorderPane P3 = new BorderPane();
-		FlowPane f1 = new FlowPane();
-		FlowPane f2 = new FlowPane();
-		Button login = new Button("Log In");
-		Button signup = new Button("Sign Up");
-		TextField autores = new TextField("AUTORES");
-		Image imagen2 = new Image(getClass().getResourceAsStream("images/tender2.png"));
-		ImageView imageView2 = new ImageView(imagen2);
+		generico.setStyle("-fx-background-color:POWDERBLUE");
 		
 		P2.setMinHeight(600);
 		P2.setMinWidth(600);
 		P3.setMinHeight(600);
 		P3.setMinWidth(600);
-	
+		
+		autores.setStyle("-fx-alignment: center; -fx-background-color:POWDERBLUE");
+		autores.setFont(Font.font("Verdana", FontWeight.LIGHT, 20));
+		
+		
 		login.setMaxWidth(Double.MAX_VALUE);
 		signup.setMaxWidth(Double.MAX_VALUE);
 		autores.setMaxWidth(Double.MAX_VALUE);
@@ -48,27 +139,54 @@ public class InterfazInicio extends Application{
 
 		f2.getChildren().add(new Label("  "));
 		f2.getChildren().add(autores);
+		f2.setStyle("-fx-background-color: gray; -fx-flow-lines-visible: true");
 		
-		P2.add(f2, 0, 0);
-		
+		P2.setTop(autores);
+		P2.setCenter(generico);
 		P3.setTop(f1);
-		P3.setCenter(imageView2);
+		P3.setCenter(imagenlabel1);
 		
 		P1.add(P2, 0, 0);
 		P1.add(P3, 1, 0);
 		P1.setStyle("-fx-background-color: white; -fx-grid-lines-visible: true");
 	
-		imageView2.setX(30);
-		imageView2.setY(30);
-		imageView2.setFitHeight(200);
-		imageView2.setFitWidth(300);
+		imagenView1.setX(30);
+		imagenView1.setY(30);
+		imagenView1.setFitHeight(200);
+		imagenView1.setFitWidth(300);
 
 		
 		Scene escena = new Scene(P1, 1200,600);
 		MyStage.setScene(escena);
 		MyStage.show();
 		
+		generico.setText(mateo.getText());
+		generico.setOnMouseClicked(cambio);
+		mateo.setOnMouseClicked(cambio);
+		santiago.setOnMouseClicked(cambio);
+		guille.setOnMouseClicked(cambio);
+		andre.setOnMouseClicked(cambio);
+		
 	}
+		
+	EventHandler<MouseEvent> cambio = new EventHandler<MouseEvent>() {
+		public void handle (MouseEvent mouseEvent) {
+			if(generico.getText().equals(mateo.getText())) {
+				generico.setText(santiago.getText());
+			}
+			else if (generico.getText().equals(santiago.getText())) {
+				generico.setText(guille.getText());
+			}
+			else if (generico.getText().equals(guille.getText())) {
+				generico.setText(andre.getText());
+			}
+			else if (generico.getText().equals(andre.getText())) {
+				generico.setText(mateo.getText());
+			}
+		}
+	};	
+	
+	
 	public static void main (String[]args) {
 		launch(args);
 	}
